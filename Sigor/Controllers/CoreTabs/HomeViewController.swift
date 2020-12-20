fb0fa91414ceed8d6589e23db3febdfe56222178
nezhitsya <huyn9295@naver.com>
2020-12-21 00:36:40 +0900

    set up post view

--- Sigor/Controllers/CoreTabs/HomeViewController.swift
+++ Sigor/Controllers/CoreTabs/HomeViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 import FirebaseAuth
 
+struct HomeFeedRenderViewModel {
+    let header: PostRenderViewModel
+    let post: PostRenderViewModel
+    let action: PostRenderViewModel
+    let commetns: PostRenderViewModel
+}
+
 class HomeViewController: UIViewController {
+    
+    private var feedRenderModels = [HomeFeedRenderViewModel]()
     
     private let tableView: UITableView = {
         let tableView = UITableView()
         tableView.register(IGFeedPostTableViewCell.self,
                            forCellReuseIdentifier: IGFeedPostTableViewCell.identifier)
+        tableView.register(IGFeedPostHeaderTableViewCell.self,
+                           forCellReuseIdentifier: IGFeedPostHeaderTableViewCell.identifier)
+        tableView.register(IGFeedPostActionsTableViewCell.self,
+                           forCellReuseIdentifier: IGFeedPostActionsTableViewCell.identifier)
+        tableView.register(IGFeedPostGeneralTableViewCell.self,
+                           forCellReuseIdentifier: IGFeedPostGeneralTableViewCell.identifier)
         return tableView
     }()
 
