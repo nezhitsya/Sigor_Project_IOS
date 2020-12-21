2823a97b15f112f09e7a2c2c6cbae04168740a31
nezhitsya <huyn9295@naver.com>
2020-12-22 01:09:03 +0900

    set up search

--- Sigor/Controllers/CoreTabs/ExploreViewController.swift
+++ Sigor/Controllers/CoreTabs/ExploreViewController.swift
@@ -11,11 +11,22 @@ class ExploreViewController: UIViewController {
     
     private let searchBar: UISearchBar = {
         let searchBar = UISearchBar()
+        searchBar.placeholder = "Search"
         searchBar.backgroundColor = .secondarySystemBackground
         return searchBar
     }()
     
+    private var models = [UserPost]()
+    
     private var collectionView: UICollectionView?
+    
+    private let dimmedView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.isHidden = true
+        view.alpha = 0
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,13 +36,64 @@ class ExploreViewController: UIViewController {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
         layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.itemSize = CGSize(width: (view.width - 4)/3, height: (view.width - 4)/3)
+        layout.minimumLineSpacing = 1
+        layout.minimumInteritemSpacing = 1
         collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView?.register(PhotoCollectionViewCell.self,
+                                 forCellWithReuseIdentifier: PhotoCollectionViewCell.identifier)
         collectionView?.delegate = self
         collectionView?.dataSource = self
         guard let collectionView = collectionView else {
             return
         }
         view.addSubview(collectionView)
+        view.addSubview(dimmedView)
+        searchBar.delegate = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView?.frame = view.bounds
+        dimmedView.frame = view.bounds
+    }
+}
+
+extension ExploreViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        didCancelSearch()
+        guard let text = searchBar.text, !text.isEmpty else {
+            return
+        }
+        query(text)
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Cancel",
+                                                            style: .plain,
+                                                            target: self,
+                                                            action: #selector(didCancelSearch))
+        dimmedView.isHidden = false
+        UIView.animate(withDuration: 0.2) {
+            self.dimmedView.alpha = 0.4
+        }
+    }
+    
+    @objc private func didCancelSearch() {
+        searchBar.resignFirstResponder()
+        navigationItem.rightBarButtonItem = nil
+        UIView.animate(withDuration: 0.2, animations: {
+            self.dimmedView.alpha = 0
+        }) { done in
+            if done {
+                self.dimmedView.isHidden = true
+            }
+        }
+    }
+    
+    private func query(_ text: String) {
+        // perform the search in the backend
     }
 }
 
@@ -41,6 +103,18 @@ extension ExploreViewController: UICollectionViewDelegate, UICollectionViewDataS
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return UICollectionViewCell()
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCollectionViewCell.identifier,
+                                                      for: indexPath) as? PhotoCollectionViewCell else {
+                                                        return UICollectionViewCell()
+                                                      }
+//        cell.configure(with: models)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        let model = models[indexPath.row]
+        let vc = PostViewController(model: model)
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
